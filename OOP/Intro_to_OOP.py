6177d629c55dbf4008ee80c6d1b53ecf41e8dd0a
Stefanos Zisidis <stefanos.zisidis@stcatherines.gr>
2026-09-22 12:18:44 +0300

    static vs non static

--- OOP/Intro_to_OOP.py
+++ OOP/Intro_to_OOP.py
@@ -1,4 +1,5 @@
 class Animal():
+    total_animal_counter = 0
     def __init__(self, name, species, age):
         self._name = name          # the leading underscore marks the attribute as private (IB convention)
         self._species = species
@@ -23,6 +24,9 @@ class Animal():
     def set_age(self):
         self._age = self._age + 1
 
+    def set_age(self, new_age):
+        self._age = new_age
+
     def set_name(self, new_name):
         self._name = new_name
 
@@ -30,7 +34,12 @@ animal1 = Animal("Flox", 1, 2)
 animal2 = Animal("Bella", "Cat", 5)
 animal3 = Animal("Mitsos", "Dog", 10)
 # print(type(animal1))
-print(animal2.get_name())
+
+animal3.set_age(12)
+print(animal3.get_age())
+animal3.set_age(23)
+print(animal3.get_age())
+
 
 print(animal2.get_name(), " is renamed to....")
 animal2.set_name("Maria")
